cb9021c5d89d9c2403ddcf60866f604f79e12147
Tadeu Vaccari <tadeuvaccari@hotmail.com>
2022-05-13 17:22:17 +0100

    Create EqualSumChecker.java

---
+++ EqualSumChecker.java
@@ -0,0 +1,13 @@
+public class EqualSumChecker {
+    
+    public static boolean hasEqualSum(int numberOne, int numberTwo, int numberThree){
+        
+        int sum = numberOne + numberTwo;
+        
+        if (sum == numberThree){
+            return true;
+        } else 
+        return false;
+    }
+ 
+}
